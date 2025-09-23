7ea10f68e54ddf8a1d4dc0ffe22949e53d1431f2
Jordan Hasgul <jkhasgul@gmail.com>
2025-09-23 17:29:12 +0100

    refactor: productionise
    Signed-off-by: Jordan Hasgul <jkhasgul@gmail.com>

--- idmint.go
+++ idmint.go
@@ -2,7 +2,6 @@ package idmint
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -15,60 +14,164 @@ type ID struct {
 	value string
 }
 
-func (id *ID) Equal(jd ID) bool {
-	return id.kind == jd.kind &&
-		id.value == jd.value
+func NewID(kind, value string) (ID, error) {
+	id := ID{
+		kind:  kind,
+		value: value,
+	}
+	err := id.Valid()
+	if err != nil {
+		return ID{}, fmt.Errorf("validating id: %w", err)
+	}
+
+	return id, nil
 }
 
-func (id *ID) Kind() string {
+func MustNewID(kind, value string) ID {
+	id, err := NewID(kind, value)
+	if err != nil {
+		err = fmt.Errorf("creating id: %w", err)
+		panic(err)
+	}
+
+	return id
+}
+
+func ParseID(idAsString string) (ID, error) {
+	kind, value, found := strings.Cut(idAsString, "_")
+	if !found {
+		return ID{}, &InvalidIDFormatError{idAsString: idAsString}
+	}
+
+	id, err := NewID(kind, value)
+	if err != nil {
+		return ID{}, fmt.Errorf("creating id: %w", err)
+	}
+
+	return id, nil
+}
+
+func MustParseID(idAsString string) ID {
+	id, err := ParseID(idAsString)
+	if err != nil {
+		err = fmt.Errorf("parsing id: %v", err)
+		panic(err)
+	}
+
+	return id
+}
+
+func (id ID) Kind() string {
 	return id.kind
 }
 
-func (id *ID) Value() string {
+func (id ID) Value() string {
 	return id.value
 }
 
-func (id *ID) String() string {
+type IDKindEmptyError struct{}
+
+func (e IDKindEmptyError) Error() string {
+	return "id kind is empty"
+}
+
+type IDKindContainsUnderscoresError struct{}
+
+func (e IDKindContainsUnderscoresError) Error() string {
+	return "id kind contains underscores"
+}
+
+type IDValueEmptyError struct{}
+
+func (e IDValueEmptyError) Error() string {
+	return "id value is empty"
+}
+
+type IDValueContainsUnderscoresError struct{}
+
+func (e IDValueContainsUnderscoresError) Error() string {
+	return "id value contains underscores"
+}
+
+func (id ID) Valid() error {
+	if id.Kind() == "" {
+		return &IDKindEmptyError{}
+	}
+
+	if strings.Contains(id.Kind(), "_") {
+		return &IDKindContainsUnderscoresError{}
+	}
+
+	if id.Value() == "" {
+		return &IDValueEmptyError{}
+	}
+
+	if strings.Contains(id.Value(), "_") {
+		return &IDValueContainsUnderscoresError{}
+	}
+
+	return nil
+}
+
+var _ fmt.Stringer = (*ID)(nil)
+
+const invalidIDString = "idmint.InvalidID"
+
+func (id ID) String() string {
+	err := id.Valid()
+	if err != nil {
+		return invalidIDString
+	}
+
 	return id.Kind() + "_" + id.Value()
+}
+
+func (id ID) Equal(jd ID) bool {
+	return id.Kind() == jd.Kind() &&
+		id.Value() == jd.Value()
 }
 
 var _ json.Marshaler = (*ID)(nil)
 
-func (id *ID) MarshalJSON() ([]byte, error) {
-	var (
-		idAsString = id.String()
-		idAsJSON   = []byte(`"` + idAsString + `"`)
-	)
+func (id ID) MarshalJSON() ([]byte, error) {
+	err := id.Valid()
+	if err != nil {
+		return nil, fmt.Errorf("validating id: %w", err)
+	}
+
+	idAsJSON := []byte(`"` + id.Kind() + `_` + id.Value() + `"`)
 	return idAsJSON, nil
+}
+
+type InvalidIDFormatError struct {
+	idAsString string
+}
+
+func (e InvalidIDFormatError) Error() string {
+	errorString := "want id to have format '<kind>_<value>' but got '%s'"
+	return fmt.Sprintf(errorString, e.idAsString)
+}
+
+func (e InvalidIDFormatError) ID() string {
+	return e.idAsString
 }
 
 var _ json.Unmarshaler = (*ID)(nil)
 
-func (id *ID) UnmarshalJSON(data []byte) error {
+func (id *ID) UnmarshalJSON(idAsJson []byte) error {
 	var idAsString string
 
-	err := json.Unmarshal(data, &idAsString)
+	err := json.Unmarshal(idAsJson, &idAsString)
 	if err != nil {
-		return fmt.Errorf("unmarshalling json to string: %w", err)
+		return fmt.Errorf("unmarshalling to string: %w", err)
 	}
 
-	idKindAndValue := strings.SplitN(idAsString, "_", 2)
-	if len(idKindAndValue) != 2 {
-		return errors.New("invalid id format, expected '<kind>_<value>'")
+	parsedID, err := ParseID(idAsString)
+	if err != nil {
+		return fmt.Errorf("parsing id: %w", err)
 	}
 
-	idKind := idKindAndValue[0]
-	if idKind == "" {
-		return errors.New("id 'kind' is empty")
-	}
-
-	idValue := idKindAndValue[1]
-	if idValue == "" {
-		return errors.New("id 'value' is empty")
-	}
-
-	id.kind = idKind
-	id.value = idValue
+	*id = parsedID
 
 	return nil
 }
@@ -94,7 +197,30 @@ type Configurer interface {
 	configure(m *Minter)
 }
 
-func NewMinter(workerID uint64, configurers ...Configurer) *Minter {
+type WorkerIDIsZeroError struct{}
+
+func (e WorkerIDIsZeroError) Error() string {
+	return "worker id is zero"
+}
+
+type WorkerIDTooLargeError struct {
+	workerID uint64
+}
+
+func (e WorkerIDTooLargeError) Error() string {
+	errorString := "worker id '%d' is greater than %d"
+	return fmt.Sprintf(errorString, e.workerID, maxWorkerID)
+}
+
+func NewMinter(workerID uint64, configurers ...Configurer) (*Minter, error) {
+	if workerID == 0 {
+		return nil, &WorkerIDIsZeroError{}
+	}
+
+	if workerID > maxWorkerID {
+		return nil, &WorkerIDTooLargeError{workerID: workerID}
+	}
+
 	minter := &Minter{
 		workerID: workerID,
 	}
@@ -102,16 +228,21 @@ func NewMinter(workerID uint64, configurers ...Configurer) *Minter {
 		configurer.configure(minter)
 	}
 
-	return minter
+	return minter, nil
+}
+
+type MinterCreatedIncorrectlyError struct{}
+
+func (e MinterCreatedIncorrectlyError) Error() string {
+	return "minter was not created using idmint.NewMinter"
 }
 
 func (m *Minter) Mint(kind string) (ID, error) {
-	m.once.Do(m.initialise)
-
-	if m.workerID > maxWorkerID {
-		errorStr := fmt.Sprintf("worker ID greater than %d", maxWorkerID)
-		return ID{}, errors.New(errorStr)
+	if m.workerID == 0 {
+		return ID{}, &MinterCreatedIncorrectlyError{}
 	}
+
+	m.once.Do(m.initialise)
 
 	now := m.timer.Time()
 	sequenceNumber, err := m.canMint(now)
@@ -129,28 +260,85 @@ func (m *Minter) Mint(kind string) (ID, error) {
 		return ID{}, fmt.Errorf("encoding value: %w", err)
 	}
 
-	return ID{
-		value: encodedValue,
-		kind:  kind,
-	}, nil
+	id, err := NewID(kind, encodedValue)
+	if err != nil {
+		return ID{}, fmt.Errorf("creating id: %w", err)
+	}
+
+	return id, nil
+}
+
+type CurrentTimeBeforeStartOfTimeError struct {
+	currentTime time.Time
+	startOfTime time.Time
+}
+
+func (e CurrentTimeBeforeStartOfTimeError) Error() string {
+	var (
+		currentTimeAsString = e.currentTime.Format(time.RFC3339)
+		startOfTimeAsString = e.startOfTime.Format(time.RFC3339)
+
+		errorStr = "current time '%s' is before start of time '%s'"
+	)
+	return fmt.Sprintf(errorStr, currentTimeAsString, startOfTimeAsString)
+}
+
+type CurrentTimeAfterEndOfTimeError struct {
+	currentTime time.Time
+	endOfTime   time.Time
+}
+
+func (e CurrentTimeAfterEndOfTimeError) Error() string {
+	var (
+		currentTimeAsString = e.currentTime.Format(time.RFC3339)
+		endOfTimeAsString   = e.endOfTime.Format(time.RFC3339)
+
+		errorString = "current time '%s' is after end of time '%s'"
+	)
+	return fmt.Sprintf(errorString, currentTimeAsString, endOfTimeAsString)
+}
+
+type TimeMovedBackwardsError struct {
+	durationMovedBackwards time.Duration
+}
+
+func (e TimeMovedBackwardsError) Duration() time.Duration {
+	return e.durationMovedBackwards
+}
+
+func (e TimeMovedBackwardsError) Error() string {
+	return "time moved backwards by " + e.durationMovedBackwards.String()
+}
+
+type SequenceNumberTooLargeError struct{}
+
+func (e SequenceNumberTooLargeError) Error() string {
+	return "sequence number is greater than " + strconv.Itoa(maxSequenceNumber)
 }
 
 func (m *Minter) canMint(now time.Time) (uint64, error) {
 	if now.Before(m.startOfTime) {
-		return 0, errors.New("before startOfTime")
+		return 0, &CurrentTimeBeforeStartOfTimeError{
+			currentTime: now,
+			startOfTime: m.startOfTime,
+		}
 	}
 
 	if now.After(m.endOfTime) {
-		return 0, errors.New("overflow")
+		return 0, &CurrentTimeAfterEndOfTimeError{
+			currentTime: now,
+			endOfTime:   m.endOfTime,
+		}
 	}
 
 	if now.Before(m.lastMintedAt) {
-		return 0, errors.New("before lastMintedAt timestamp")
+		durationMovedBackwards := m.lastMintedAt.Sub(now)
+		return 0, &TimeMovedBackwardsError{durationMovedBackwards: durationMovedBackwards}
 	}
 
 	sequenceNumber := m.getNextSequenceNumber(now)
 	if sequenceNumber > maxSequenceNumber {
-		return 0, errors.New("sequence number greater than max sequence")
+		return 0, &SequenceNumberTooLargeError{}
 	}
 
 	return sequenceNumber, nil
@@ -160,24 +348,26 @@ func (m *Minter) getNextSequenceNumber(now time.Time) uint64 {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
-	m.sequenceNumber++
 	if now.After(m.lastMintedAt) {
 		m.lastMintedAt = now
 		m.sequenceNumber = 0
 	}
 
-	return m.sequenceNumber
+	sequenceNumber := m.sequenceNumber
+	m.sequenceNumber++
+
+	return sequenceNumber
 }
 
 const (
-	numTimeBits = uint64(42)
-	maxTime     = (1 << numTimeBits) - 1
+	numBitsForTimestamp = uint64(42)
+	maxTimestamp        = (1 << numBitsForTimestamp) - 1
 
-	numWorkerIDBits = uint64(10)
-	maxWorkerID     = (1 << numWorkerIDBits) - 1
+	numBitsForWorkerID = uint64(10)
+	maxWorkerID        = (1 << numBitsForWorkerID) - 1
 
-	numSequenceNumberBits = uint64(12)
-	maxSequenceNumber     = (1 << numSequenceNumberBits) - 1
+	numBitsForSequenceNumber = uint64(12)
+	maxSequenceNumber        = (1 << numBitsForSequenceNumber) - 1
 )
 
 func (m *Minter) doMint(now time.Time, sequenceNumber uint64) uint64 {
@@ -185,47 +375,37 @@ func (m *Minter) doMint(now time.Time, sequenceNumber uint64) uint64 {
 
 	rawValue |= uint64(now.Sub(m.startOfTime).
 		Milliseconds())
-	offset := 64 - numTimeBits
-	rawValue <<= offset
 
+	rawValue <<= numBitsForWorkerID
 	rawValue |= m.workerID
-	offset -= numWorkerIDBits
-	rawValue <<= offset
 
+	rawValue <<= numBitsForSequenceNumber
 	rawValue |= sequenceNumber
-	offset -= numSequenceNumberBits
-	rawValue <<= offset
 
 	return rawValue
 }
 
-var defaultEpoch = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC).
-	Truncate(time.Millisecond)
+var defaultStartOfTime = time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
 
 func (m *Minter) initialise() {
 	if m.startOfTime.IsZero() {
-		m.startOfTime = defaultEpoch
+		m.startOfTime = defaultStartOfTime
 	}
-	m.endOfTime = m.startOfTime.Add(maxTime * time.Millisecond).
-		Truncate(time.Millisecond)
+	m.startOfTime = m.startOfTime.Truncate(time.Second)
+	m.endOfTime = m.startOfTime.Add(maxTimestamp * time.Millisecond)
 
-	timer := m.timer
-	if timer == nil {
-		timer = TimeFunc(time.Now)
+	if m.timer == nil {
+		m.timer = stdTimer{}
 	}
-	m.timer = TimeFunc(func() time.Time {
-		now := timer.Time().
-			Truncate(time.Millisecond)
-		return now
-	})
+	m.timer = truncatingTimer{
+		timer:    m.timer,
+		duration: time.Millisecond,
+	}
 
 	m.lastMintedAt = m.timer.Time()
 
 	if m.encoder == nil {
-		noOpEncoder := func(s string) (string, error) {
-			return s, nil
-		}
-		m.encoder = EncoderFunc(noOpEncoder)
+		m.encoder = noOpEncoder{}
 	}
 }
 
@@ -249,6 +429,23 @@ func (c *withTimerConfigurer) configure(m *Minter) {
 
 func WithTimer(timer Timer) Configurer {
 	return &withTimerConfigurer{timer: timer}
+}
+
+type stdTimer struct{}
+
+var _ Timer = (*stdTimer)(nil)
+
+func (s stdTimer) Time() time.Time {
+	return time.Now()
+}
+
+type truncatingTimer struct {
+	timer    Timer
+	duration time.Duration
+}
+
+func (t truncatingTimer) Time() time.Time {
+	return t.timer.Time().Truncate(t.duration)
 }
 
 type withEpochConfigurer struct {
@@ -283,4 +480,10 @@ func (c *withEncoderConfigurer) configure(m *Minter) {
 
 func WithEncoder(encoder Encoder) Configurer {
 	return &withEncoderConfigurer{encoder: encoder}
+}
+
+type noOpEncoder struct{}
+
+func (e noOpEncoder) Encode(s string) (string, error) {
+	return s, nil
 }
